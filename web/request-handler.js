705c317e89317f1722e7a0f43a55ba67678454ff
wyattfry <wyattfry@gmail.com>
2018-02-07 15:13:58 -0800

    Rendered the index to html.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,18 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var http = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  fs.readFile(archive.paths.siteAssets + "/index.html", "utf8", (err, data) => {
+    if (err) { console.error(err); }
+    res.writeHead(200, http.headers);
+    res.write(data);
+    res.end();
+
+  });
+  
+  //res.end(archive.paths.siteAssetslist);
 };
